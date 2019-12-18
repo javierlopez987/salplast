5dddfb614b6e0c752211df7cbe982d2b617fda07
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-12-18 16:41:50 -0300

    Database.php modified

--- DB/Database.php
+++ DB/Database.php
@@ -6,7 +6,7 @@ class Database {
 
     private function __construct() {
         try {
-        $this->connection = new PDO('mysql:host=10.0.10.39;dbname=salplast_db_cancionero;charset=utf8', 'salplast_root', 'Federico_21');
+        $this->connection = new PDO('mysql:host=10.0.10.39;dbname=salplast_db_cancionero;charset=utf8', 'salplast_root@nefertiti.servidoraweb.net', 'Federico_21');
         } catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
             die;
