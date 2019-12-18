b3ab5040b4cc4bf26d5b09e52d4665462e686b44
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-12-18 18:27:36 -0300

    Database.php modified, user modified 8

--- DB/Database.php
+++ DB/Database.php
@@ -8,8 +8,8 @@ class Database {
 
     private function __construct() {
         try {
-            $this->usuario = 'root';
-            $this->clave = '';
+            $this->usuario = 'salplast_admin';
+            $this->clave = 'Administrador_22';
             $this->connection = new PDO('mysql:host=10.0.10.39;dbname=salplast_db_cancionero;port=3306', $this->usuario, $this->clave);
         } catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
