f6a287f7f20447431e647c402a4f6d6df6711fb0
Javier Lopez Ferreira <javierlopez987@gmail.com>
2019-12-26 10:45:51 -0300

    DOMContentLoaded added - comments.js - master

--- js/comment.js
+++ js/comment.js
@@ -1,74 +1,76 @@
-"use strict"
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict"
 
-let app = new Vue({
-    el: '#template-vue-comment',
-    data: {
-        subtitle: "Comentarios",
-        comments: [],
-        auth: false,
-        promedio: 0
-    }/* ,
-    methods: {
-        borrar: deleteComment()
+    let app = new Vue({
+        el: '#template-vue-comment',
+        data: {
+            subtitle: "Comentarios",
+            comments: [],
+            auth: false,
+            promedio: 0
+        }/* ,
+        methods: {
+            borrar: deleteComment()
+        } */
+    })
+
+    function getComments(cancion) {
+        fetch("api/comments/" + cancion, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(response => response.json())
+        .then(comments => {
+            let suma = 0;
+            let cuenta = 0;
+            for (const comment of comments) {
+                suma += parseInt(comment.valoracion);
+                cuenta++;
+            }
+            let promedio = suma/cuenta;
+            console.log(comments);
+            app.comments = comments;
+            app.promedio = promedio;
+        })
+        .catch(error => console.log(error));
+    }
+
+    let cancion = document.querySelector("#idCancion").value;
+    getComments(cancion);
+
+    //document.querySelector("#form-comment").addEventListener('submit', addComment);
+
+    /* function addComment(e) {
+        e.preventDefault();
+        
+        let data = {
+            mensaje:  document.querySelector("input[name=mensaje]").value,
+            valoracion:  document.querySelector("input[name=valoracion]").value,
+            cancion:  document.querySelector("input[name=cancion]").value,
+            user:  document.querySelector("input[name=user]").value,
+        }
+        fetch('api/comments', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},       
+            body: JSON.stringify(data) 
+        })
+        .then(response => {
+            getComments(data.cancion);
+        })
+        .catch(error => console.log(error));
+    } */
+
+    /* function deleteComment(e) {
+        e.preventDefault();
+        let id = document.querySelector("#btn-borrar").value;
+        fetch('api/comments/id', {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'},       
+            body: JSON.stringify(data) 
+        })
+        .then(response => {
+            getComments(data.cancion);
+        })
+        .catch(error => console.log(error));
     } */
 })
-
-function getComments(cancion) {
-    fetch("api/comments/" + cancion, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}
-     })
-    .then(response => response.json())
-    .then(comments => {
-        let suma = 0;
-        let cuenta = 0;
-        for (const comment of comments) {
-            suma += parseInt(comment.valoracion);
-            cuenta++;
-        }
-        let promedio = suma/cuenta;
-        console.log(comments);
-        app.comments = comments;
-        app.promedio = promedio;
-    })
-    .catch(error => console.log(error));
-}
-
-let cancion = document.querySelector("#idCancion").value;
-getComments(cancion);
-
-//document.querySelector("#form-comment").addEventListener('submit', addComment);
-
-/* function addComment(e) {
-    e.preventDefault();
-    
-    let data = {
-        mensaje:  document.querySelector("input[name=mensaje]").value,
-        valoracion:  document.querySelector("input[name=valoracion]").value,
-        cancion:  document.querySelector("input[name=cancion]").value,
-        user:  document.querySelector("input[name=user]").value,
-    }
-    fetch('api/comments', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},       
-        body: JSON.stringify(data) 
-     })
-     .then(response => {
-        getComments(data.cancion);
-     })
-     .catch(error => console.log(error));
-} */
-
-/* function deleteComment(e) {
-    e.preventDefault();
-    let id = document.querySelector("#btn-borrar").value;
-    fetch('api/comments/id', {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'},       
-        body: JSON.stringify(data) 
-     })
-     .then(response => {
-        getComments(data.cancion);
-     })
-     .catch(error => console.log(error));
-} */
